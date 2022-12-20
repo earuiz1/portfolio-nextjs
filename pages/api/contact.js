b888cfcd9a5e34b112c6b40040e649d2a166669f
earuiz1 <earuiz1196@gmail.com>
2022-12-19 21:45:17 -0600

    Imported both the mailOptions and transporter variables so they can be of use on the contact endpoint file. Now, the transporter sendMail method sends an email with a subject, text(string of the email) and an html property

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,12 +1,31 @@
-const handler = (req, res) => {
+import { mailOptions, transporter } from "../../config/nodemailer";
+
+/**
+ * It checks if the request method is POST and if the data is valid, then it sends an email using the
+ * data provided
+ */
+const handler = async (req, res) => {
   console.log(req.body);
+
   if (req.method === "POST") {
     const data = req.body;
     if (!data.name || !data.email || !data.subject || !data.message) {
       return res.status(400).json({ message: "Bad Request!" });
     }
+    try {
+      await transporter.sendMail({
+        ...mailOptions,
+        subject: data.subject,
+        text: "This is a test string",
+        html: `<p>Name: ${data.name}</p><p>Email: ${data.email}</p><p>Email: ${data.message}</p>`,
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(400).json({ message: error.message });
+    }
   }
-  return res.status(400).json({ message: "Bad Request!" });
+
+  res.status(200).json({ name: "John Doe" });
 };
 
 export default handler;
