40147b96325c3e6d3fff642c6012cce2eef4ca00
earuiz1 <earuiz1196@gmail.com>
2023-01-17 14:32:35 -0600

    Deleted workflows

--- components/AboutMe.jsx
+++ components/AboutMe.jsx
@@ -15,7 +15,7 @@ const AboutMe = () => {
           </h2>
         </div>
         <div
-          className={`flex flex-col items-center gap-5 md:flex-row md:justify-center lg:flex-row lg:justify-center mx-4 my-40 transition-all duration-[2000ms] ${
+          className={`flex flex-col items-center gap-5 md:flex-row md:justify-center lg:flex-row lg:justify-center mx-4 lg:my-40 md:my-40 my-20 transition-all duration-[2000ms] ${
             isVisible ? "opacity-100 " : "opacity-0"
           }`}
           ref={myRef}
@@ -30,17 +30,21 @@ const AboutMe = () => {
           <div className="flex flex-col gap-2">
             <p className="text-sm md:text-lg lg:text-xl">
               I am a Computer Science Grad with a Masters in Information
-              Technology at The University of Texas Rio Grande Valley (UTRGV). I
-              enjoy building web applications and exploring new frameworks
-              related to web development. My end goal is to always give my best
-              in everything I do and enjoy the process.
+              Technology from The University of Texas Rio Grande Valley. I have
+              2 years of experience working as a Web Developer/ Digital
+              Marketing Assistant at UTRGV.
             </p>
             <p className="text-sm md:text-lg lg:text-xl">
-              I have 2 years of experience working as a Web Developer/ Digital
-              Marketing Assistant at UTRGV. During this process I used tools
-              such as Unbounce and Microsoft Power Bi and learned a lot about
-              web development, website optimization and digital marketing &
-              analytics.
+              My educational background and professional experience have given
+              me a strong foundation in web development and digital marketing,
+              as well as an understanding of the latest technologies and trends
+              in the industry. I am passionate about using my skills to create
+              engaging and user-friendly websites, and I'm constantly seeking
+              new opportunities to learn and grow in my field. I'm a fast
+              learner, hard worker, and team player who is dedicated to
+              delivering high-quality results. I am excited to continue my
+              career as a web developer and digital marketer, and look forward
+              to the challenges and opportunities that lie ahead.
             </p>
             <p className="text-sm md:text-lg lg:text-xl">
               Some of my hobbies are: playing video games, reading stuff about
